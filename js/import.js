be8488853e90dcdf7615e2db5a8f200974e34f04
radostBeZuma <romahasasha@yandex.ru>
2022-05-20 19:25:11 +0300

    Сделал загрузку блоков, которые скорее всего не понадобятся Свете

---
+++ js/import.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+	fetch("./header.html")
+		.then(response => {
+			return response.text()
+		})
+		.then(data => {
+			document.querySelector("header").innerHTML = data;
+		});
+
+	// fetch("./footer.html")
+	//   .then(response => {
+	//     return response.text()
+	//   })
+	//   .then(data => {
+	//     document.querySelector("footer").innerHTML = data;
+	//   });
+
+
+});
